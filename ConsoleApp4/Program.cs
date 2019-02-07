63ae94ef686ecd1013a7dd320cd3cef3a00e62f0
visualstudio <visualstudio@230-PC42>
2019-02-07 15:46:39 -0500

    COMMIT

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,12 +4,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace ConsoleApp3
 {
     class Program
     {
+        private readonly object myfavouritevariables;
+
         static void Main(string[] args)
         {
+            var a = new TestQuestion2();
+            a.PlayingWithForLoops();
+        }
+        class TestQuestion2
+        {
+            public void PlayingWithForLoops()
+            {
+                for (; mymethod();)
+                {
+                    if (myfavouritevariables > 10)
+                    {
+                        Console.WriteLine("i am in for loops");
+                        break;
+                    }
+                }
+                Console.WriteLine("oh no i have to go through this stupoid loop again");
+            }
+        }
+        public bool mymethod()
+        {
+            myfavouritevariables
+        return true;
         }
     }
+
+
+
+
+    class dog
+    {
+        public dog(string name, string breed)
+        {
+            dog_name = name;
+            dog_breed = breed;
+        }
+        public string dog_name;
+        public string dog_breed;
+        public dog next_dog;
+        public dog previous_dog;
+
+    }
+    class birthday_party
+    {
+        public dog fifo;
+        public dog peanut;
+        public dog clarence;
+        public dog roy;
+        public dog head;
+        public void pea();
+    }
+
+
 }
+}
+
